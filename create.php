46b61f9e531b4b489a4cf0770b85ab26e049a211
DeenoDev <deyanoklaasen@outlook.com>
2023-08-12 23:26:32 +0200

    create.php file completed. Submit form completed.

---
+++ create.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-CRUD-Project</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container my-5">
+        <h2>New Client</h2>
+        <form method="post">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Name</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name" value="">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Email</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="email" value="">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Phone</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="phone" value="">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Address</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="address" value="">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button class="btn btn-primary" type="submit">Submit</button>
+                </div>
+
+                <div class="col-sm-3 d-grid">
+                    <a href="/PHP-CRUD-Project/index.php" class="btn btn-outline-primary" role="button">Cancel</a>
+                </div>
+            </div>
+
+        </form>
+
+    </div>
+</body>
+</html>
