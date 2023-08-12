41902b2fe8923c8498388ed794055693a7cf5ad9
DeenoDev <deyanoklaasen@outlook.com>
2023-08-12 18:57:17 +0200

    Fixed table layout bug by adding class table to table.

--- index.php
+++ index.php
@@ -5,14 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Shop</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container my-5">
         <h2>List Of Clients</h2>
         <a class="btn btn-primary" role="button" href="/PHP-CRUD-PROJECT/create.php">New Client</a>
         <br>
-        <table>
-            <thead class="my-5">
+        <table class="table">
+            <thead>
                 <tr>
                     <th>ID</th>
                     <th>Name</th>
@@ -69,6 +70,6 @@
             </tbody>
         </table>
     </div>
-    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
