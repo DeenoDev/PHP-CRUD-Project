bf4f71f83b3bc132fb388b779584de8d9aad5117
DeenoDev <deyanoklaasen@outlook.com>
2023-08-12 17:56:34 +0200

    Created Header and New Client Button.

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
 </head>
 <body>
+    <div class="container my-5">
+        <h2>List Of Clients</h2>
+        <a class="btn btn-primary" role="button" href="/PHP-CRUD-PROJECT/create.php">New Client</a>
+    </div>
     
 </body>
 </html>
