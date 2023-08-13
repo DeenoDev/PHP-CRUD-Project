a3144b9844a1fefd47ecd5866eb4c77f21ee4ab3
DeenoDev <deyanoklaasen@outlook.com>
2023-08-13 18:02:35 +0200

    Added PHP variables to edit.php page.

---
+++ edit.php
@@ -0,0 +1,101 @@
+<?php
+$id = "";
+$name = "";
+$email = "";
+$phone = "";
+$address = "";
+
+$errorMessage = "";
+$successMessage = "";
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-CRUD-Project</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <div class="container my-5">
+        <h2>New Client</h2>
+
+        <?php
+        if ( !empty($errorMessage)) {
+            echo "
+            <div class='alert alert-warning alert-dismissible fade show' role='alert'>
+                <strong>$errorMessage</strong>
+                <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+            </div>  
+            ";
+        }
+        ?>
+
+
+
+        <form method="post">
+            <input type="hidden" value="<?php echo $id; ?>">
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Name</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="name" value="<?php echo $name; ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Email</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="email" value="<?php echo $email; ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Phone</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="phone" value="<?php echo $phone; ?>">
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label class="col-sm-3 col-form-label" for="">Address</label>
+                <div class="col-sm-6">
+                    <input type="text" class="form-control" name="address" value="<?php echo $address; ?>">
+                </div>
+            </div>
+
+            <?php
+            if (!empty($successMessage)) {
+                echo "
+                <div class='row mb-3'>
+                  <div class='offset-sm-3 col-sm-6'>
+                    <div class='alert alert-success alert-dismissible fade show' role='alert'>
+                      <strong>$successMessage</strong>
+                      <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                    </div>
+                   </div>
+                  </div>  
+                
+                ";
+            }
+            ?>
+
+            <div class="row mb-3">
+                <div class="offset-sm-3 col-sm-3 d-grid">
+                    <button class="btn btn-primary" type="submit">Submit</button>
+                </div>
+
+                <div class="col-sm-3 d-grid">
+                    <a href="/PHP-CRUD-Project/index.php" class="btn btn-outline-primary" role="button">Cancel</a>
+                </div>
+            </div>
+
+        </form>
+
+    </div>
+</body>
+</html>
