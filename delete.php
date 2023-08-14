6ae7406da18bcba389c37f8306e37e8d8097383c
DeenoDev <deyanoklaasen@outlook.com>
2023-08-14 20:44:31 +0200

    Created connection to databse.

--- delete.php
+++ delete.php
@@ -1,6 +1,14 @@
 <?php
 if ( isset($_GET["id"]) ) {
     $id = $_GET["id"];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "myshop";
+
+    //Create connection
+    $connection = new mysqli($servername, $username, $password, $database);
 }
 
 
